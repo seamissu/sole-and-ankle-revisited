5480dd7c4db5c78bb5e8a58edb6168d2aa1062ea
Jingning <seamissu@gmail.com>
2023-10-16 23:30:19 +1100

    Tweaks to main view

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -14,7 +14,20 @@ const ShoeIndex = ({ sortId, setSortId }) => {
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
+          <div>
+            <BreadWrapper>
+              <Breadcrumbs>
+                <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+                <Breadcrumbs.Crumb href="/sale">
+                  Sale
+                </Breadcrumbs.Crumb>
+                <Breadcrumbs.Crumb href="/sale/shoes">
+                  Shoes
+                </Breadcrumbs.Crumb>
+              </Breadcrumbs>
+            </BreadWrapper>
+            <Title>Running</Title>
+          </div>
           <Select
             label="Sort"
             value={sortId}
@@ -51,6 +64,11 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+    flex-direction: row;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,11 +79,22 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media ${QUERIES.tabletAndDown} {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const BreadWrapper = styled.div`
+  display: none;
+  @media ${QUERIES.tabletAndDown} {
+    display: block;
+  }
 `;
 
 export default ShoeIndex;
